4bdfa3b33a13aba63ad88c3bc7b6dc0b86f9df25
dogacaltin <dogac.altin@sabanciuniv.edu>
2025-10-04 17:45:46 +0300

    routera

--- app/routers/__init__.py
+++ app/routers/__init__.py
@@ -1,6 +1,6 @@
 # Bu dosya, "app.routers" paketinden nelerin import edilebileceğini tanımlar.
 # main.py'ın tüm router'ları bulabilmesi için hepsini burada listelemeliyiz.
 
-from . import auth
+from . import user
 from . import order
 from . import expense
